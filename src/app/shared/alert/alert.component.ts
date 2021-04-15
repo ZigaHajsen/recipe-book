fd109aed9a936bad1fe6a82bb5d8459de67f6533
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-04-16 00:09:40 +0200

    added alert component functionality

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+  styleUrls: ['./alert.component.css'],
 })
 export class AlertComponent implements OnInit {
+  @Input() message: string;
+  @Output() close = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClose() {
+    this.close.emit();
   }
-
 }
